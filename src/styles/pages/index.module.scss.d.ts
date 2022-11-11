4772a267a59c564f38a36c49b89c1510ea42436a
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2022-11-11 18:32:44 +0900

    chore: yarn fix

--- src/styles/pages/index.module.scss.d.ts
+++ src/styles/pages/index.module.scss.d.ts
@@ -1,10 +1,9 @@
 declare const styles: {
-  readonly "container": string;
-  readonly "main": string;
-  readonly "pageTitle": string;
-  readonly "message": string;
-  readonly "cards": string;
-  readonly "footer": string;
-};
-export = styles;
-
+  readonly container: string
+  readonly main: string
+  readonly pageTitle: string
+  readonly message: string
+  readonly cards: string
+  readonly footer: string
+}
+export = styles
